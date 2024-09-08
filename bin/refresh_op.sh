f2ea35588e18dd46e455e20f1a9aae8dfbea8af2
alex-1900 <omytty@126.com>
2024-09-08 14:49:57 +0800

    update

---
+++ bin/refresh_op.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -f ~/.op_session ]; then
+    export OP_SESSION=$(cat ~/.op_session)
+fi
+
+if ! op whoami > /dev/null 2>&1; then
+    export OP_SESSION=$(op signin --raw)
+    echo "$OP_SESSION" > ~/.op_session
+fi
+
+op vault list
